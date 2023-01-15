866d98b41af78da64982c3ea44418d6fb93387d7
Matt March <matthew.march@ghyston.com>
2023-01-15 14:41:35 +0000

    Take postcode from URL parameters

--- lib/bin_day_as_a_service_web/controllers/page_controller.ex
+++ lib/bin_day_as_a_service_web/controllers/page_controller.ex
@@ -2,11 +2,15 @@ defmodule BinDayAsAServiceWeb.PageController do
   use BinDayAsAServiceWeb, :controller
 
   def index(conn, _params) do
+    render(conn, "index.html")
+  end
+
+  def postcode_search(conn, %{"postcode" => postcode}) do
     bin_details =
-      "postcode here"
+      postcode
       |> CouncilBinsSite.get_collection_dates()
       |> inspect()
 
-    render(conn, "index.html", bin_detail_string: bin_details)
+    render(conn, "postcode_search.html", bin_detail_string: bin_details)
   end
 end
--- lib/bin_day_as_a_service_web/router.ex
+++ lib/bin_day_as_a_service_web/router.ex
@@ -18,6 +18,7 @@ defmodule BinDayAsAServiceWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/:postcode", PageController, :postcode_search
   end
 
   # Other scopes may use custom stacks.
